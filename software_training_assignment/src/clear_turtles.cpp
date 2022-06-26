4d318c9d4c9c7816b2cd38a5e0be9ef5ddf159a9
Nick-palmar <nicolas.palmar8@gmail.com>
2022-06-26 23:27:34 +0000

    change clear turtle client call into service

--- software_training_assignment/src/clear_turtles.cpp
+++ software_training_assignment/src/clear_turtles.cpp
@@ -9,27 +9,41 @@
 
 #include <software_training_assignment/visibility.h>
 #include <turtlesim/srv/kill.hpp>
+#include <std_srvs/srv/trigger.hpp>
 
 using namespace std::chrono_literals;
 
 namespace composition {
 
+using namespace std::placeholders;
 class clear_turtles : public rclcpp::Node {
 public:
   SOFTWARE_TRAINING_PUBLIC
   explicit clear_turtles(const rclcpp::NodeOptions &options): Node("clear_turtle_node", options) {
       // create the client to call the kill service
       client = create_client<turtlesim::srv::Kill>("/kill");
+      // create a service to call the kill client
+      clear_server = create_service<std_srvs::srv::Trigger>("remove_all_turtles", std::bind(&clear_turtles::clear, this, _1, _2));
+      // create a service to add new turtles when spawning (to know which turtles to kill when clearing)
+      add_name_server = create_service<turtlesim::srv::Kill>("add_turtle_name", std::bind(&clear_turtles::add_name, this, _1, _2));
   }
 
 private:
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr clear_server;
   rclcpp::Client<turtlesim::srv::Kill>::SharedPtr client;
+  // create a service to add turtles to the list of turtles
+  rclcpp::Service<turtlesim::srv::Kill>::SharedPtr add_name_server;
 
-  // all the turtles including the default turtle
-  std::vector<std::string> turtle_names = {"turtle1", "moving_turtle",
-                                           "stationary_turtle"};
+  // clear the initial turtle which is launched with the turtle node
+  std::vector<std::string> turtle_names = {"turtle1"};
+  // , "moving_turtle", "stationary_turtle"};
 
   SOFTWARE_TRAINING_LOCAL
+
+  void add_name(const std::shared_ptr<turtlesim::srv::Kill::Request> req,
+    const std::shared_ptr<turtlesim::srv::Kill::Response> /*resp */) {
+      turtle_names.push_back(req->name);
+    }
 
   void kill_callback(rclcpp::Client<turtlesim::srv::Kill>::SharedFuture resp) {
       (void)resp;
@@ -37,24 +51,32 @@ private:
       // stop executing callback
       rclcpp::shutdown();
   }
-  void kill() {
+  
+  void clear(const std::shared_ptr<std_srvs::srv::Trigger::Request> req,
+    std::shared_ptr<std_srvs::srv::Trigger::Response> resp) {
+    
+    (void)req;
     if (!client->wait_for_service(2s)) {
           if(!rclcpp::ok()) {
               RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for kill service, exiting");
+              resp->success = false;
               return;
           }
           RCLCPP_INFO(this->get_logger(), "Service not avaliable");
+          resp->success = false;
           return;
     }
 
     for (auto name: turtle_names) {
-        using namespace std::placeholders;
-        auto req = std::make_shared<turtlesim::srv::Kill::Request>();
-        req->name = name;
-        auto res = client->async_send_request(req, std::bind(&clear_turtles::kill_callback, this, _1));
+        auto kill_req = std::make_shared<turtlesim::srv::Kill::Request>();
+        kill_req->name = name;
+        // send request to kill turtle
+        // auto res = client->async_send_request(kill_req, std::bind(&clear_turtles::kill_callback, this, _1));
+        auto res = client->async_send_request(kill_req);
     }
-    
-
+    // remove all turtles after clearing them to update the known turtles
+    turtle_names.clear();
+    resp->success = true;
     
   }
 };
